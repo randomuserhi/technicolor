9876be594fad81de2002a14344d33056ece9ad41
Christopher Teo <40913834+randomuserhi@users.noreply.github.com>
2024-08-09 19:25:56 +0100

    adjustments

--- src/routes/flower.ts
+++ src/routes/flower.ts
@@ -134,8 +134,6 @@ export const flower = Macro(class Flower extends MacroElement {
         }));
         this.composer.addPass(new OutputPass());
 
-        (window as any).bokeh = this.bokeh;
-
         const pointLight = new PointLight(0xFFFFFF, 10, undefined, 0.25);
         this.camera.root.add(pointLight);
         pointLight.position.set(0, 0, 0);
@@ -344,13 +342,8 @@ export const flower = Macro(class Flower extends MacroElement {
 
     private resize() {
         const computed = getComputedStyle(this.canvas);
-        let width = parseInt(computed.width);
-        let height = parseInt(computed.height);
-
-        if (mobile) {
-            width *= 2;
-            height *= 2;
-        }
+        const width = parseInt(computed.width);
+        const height = parseInt(computed.height);
 
         this.camera.resize(width, height);
         this.renderer.setPixelRatio(window.devicePixelRatio);
@@ -358,6 +351,8 @@ export const flower = Macro(class Flower extends MacroElement {
         this.bloom.setSize(width, height);
         this.bokeh.setSize(width, height);
         this.composer.setSize(width, height);
+
+        this.bokeh.uniforms.aspect.value = 16 / 9;
 
         if (width > 1000) {
             this.root.position.x = -130 * SCENE_SCALE;
