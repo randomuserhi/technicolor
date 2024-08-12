3c3433bb4ed3bbd8bd3f48f8130264afc7e1ea24
Christopher Teo <40913834+randomuserhi@users.noreply.github.com>
2024-08-12 13:10:03 +0100

    Updates

--- routes/flower.js
+++ routes/flower.js
@@ -83,7 +83,7 @@ export const flower = Macro(class Flower extends MacroElement {
         this.composer.addPass(this.bokeh = new BokehPass(this.scene, this.camera.root, {
             focus: 60 * SCENE_SCALE,
             aperture: 0.025,
-            maxblur: 0.01,
+            maxblur: 0.004,
         }));
         this.composer.addPass(new OutputPass());
         const pointLight = new PointLight(0xFFFFFF, 10, undefined, 0.25);
@@ -93,7 +93,7 @@ export const flower = Macro(class Flower extends MacroElement {
         light.position.set(0, 10, 0);
         this.scene.add(light);
         const root = this.root = new Group();
-        root.position.set(-120, -135, 0).multiplyScalar(SCENE_SCALE);
+        root.position.set(-120, -155, 0).multiplyScalar(SCENE_SCALE);
         root.rotation.set(45 * Math.deg2rad, -55 * Math.deg2rad, 0, "YXZ");
         this.scene.add(root);
         const flower = this.flower = new Group();
@@ -146,6 +146,105 @@ export const flower = Macro(class Flower extends MacroElement {
                 inner.quaternion.copy(rotation);
                 outer.quaternion.copy(rotation);
             }
+        }).then(() => {
+            loadGLTF("./js3party/models/petal-small.glb").then((factory) => {
+                for (let i = 0; i < 3; ++i) {
+                    const inner = factory();
+                    inner.scale.set(SCENE_SCALE, SCENE_SCALE, SCENE_SCALE);
+                    const innerMaterial = new MeshPhysicalMaterial({
+                        color: 0x3f7ee8,
+                        emissive: 0x3f7ee8,
+                        emissiveIntensity: 2.5,
+                        ior: 1.5,
+                        specularIntensity: 1,
+                        specularColor: 0xffffff,
+                        side: DoubleSide
+                    });
+                    inner.traverse((obj) => {
+                        const mesh = obj;
+                        if (mesh.isMesh === true) {
+                            mesh.material = innerMaterial;
+                            mesh.scale.set(0.95, 0.95, 0.95);
+                            this.gears.push(mesh);
+                        }
+                    });
+                    flower.add(inner);
+                    const outer = factory();
+                    outer.scale.set(SCENE_SCALE, SCENE_SCALE, SCENE_SCALE);
+                    const outerMaterial = Object.assign(new MeshTransmissionMaterial(10), {
+                        clearcoat: 1,
+                        clearcoatRoughness: 0.1,
+                        transmission: 1,
+                        chromaticAberration: 1,
+                        anisotrophicBlur: 0.3,
+                        roughness: 0,
+                        thickness: 4.5,
+                        ior: 1.5,
+                        side: DoubleSide
+                    });
+                    outer.traverse((obj) => {
+                        const mesh = obj;
+                        if (mesh.isMesh === true) {
+                            mesh.material = outerMaterial;
+                            this.gears.push(mesh);
+                        }
+                    });
+                    flower.add(outer);
+                    const rotation = new Quaternion();
+                    rotation.setFromEuler(new Euler(0, 0, i * 120 * Math.deg2rad, "YXZ"));
+                    inner.quaternion.copy(rotation);
+                    outer.quaternion.copy(rotation);
+                }
+            });
+            loadGLTF("./js3party/models/petal.glb").then((factory) => {
+                for (let i = 0; i < 3; ++i) {
+                    const inner = factory();
+                    inner.scale.set(SCENE_SCALE, SCENE_SCALE, SCENE_SCALE);
+                    const innerMaterial = new MeshPhysicalMaterial({
+                        color: 0x3f7ee8,
+                        emissive: 0x3f7ee8,
+                        emissiveIntensity: 2.5,
+                        ior: 1.5,
+                        specularIntensity: 1,
+                        specularColor: 0xffffff,
+                        side: DoubleSide
+                    });
+                    inner.traverse((obj) => {
+                        const mesh = obj;
+                        if (mesh.isMesh === true) {
+                            mesh.material = innerMaterial;
+                            mesh.scale.set(0.95, 0.95, 0.95);
+                            this.gears.push(mesh);
+                        }
+                    });
+                    flower.add(inner);
+                    const outer = factory();
+                    outer.scale.set(SCENE_SCALE, SCENE_SCALE, SCENE_SCALE);
+                    const outerMaterial = Object.assign(new MeshTransmissionMaterial(10), {
+                        clearcoat: 1,
+                        clearcoatRoughness: 0.1,
+                        transmission: 1,
+                        chromaticAberration: 1,
+                        anisotrophicBlur: 0.3,
+                        roughness: 0,
+                        thickness: 4.5,
+                        ior: 1.5,
+                        side: DoubleSide
+                    });
+                    outer.traverse((obj) => {
+                        const mesh = obj;
+                        if (mesh.isMesh === true) {
+                            mesh.material = outerMaterial;
+                            this.gears.push(mesh);
+                        }
+                    });
+                    flower.add(outer);
+                    const rotation = new Quaternion();
+                    rotation.setFromEuler(new Euler(0, 0, i * 120 * Math.deg2rad + 60 * Math.deg2rad, "YXZ"));
+                    inner.quaternion.copy(rotation);
+                    outer.quaternion.copy(rotation);
+                }
+            });
         });
         loadGLTF("./js3party/models/pushrod.glb").then((factory) => {
             const model = factory();
@@ -240,19 +339,21 @@ export const flower = Macro(class Flower extends MacroElement {
         this.camera.root.position.copy(position);
         this.camera.root.rotation.set(rotation.x, rotation.y, rotation.z, "YXZ");
         const background = [
-            ["T", "RANSFORM", 8, 0xff0000],
-            ["E", "NGINEER", 9, 0xff5300],
-            ["C", "HANGE", 10, 0xffa500],
-            ["H", "YBRID", 11, 0xffd200],
-            ["N", "ATURE", 10, 0xffff00],
-            ["I", "NVENT", 11, 0x80c000],
-            ["C", "ATALYSE", 9, 0x008000],
-            ["O", "PERATE", 9, 0x004080],
-            ["L", "EAGUE", 9, 0x0000ff],
-            ["O", "BEISM", 10, 0x2600c1],
-            ["R", "UMBLE", 10, 0x4b0082],
+            ["T", "RANSFORM", 10, 0xec1313],
+            ["E", "NGINEER", 10, 0xec5b13],
+            ["C", "HANGE", 10, 0xeca013],
+            ["H", "YBRID", 10, 0xecc413],
+            ["N", "ATURE", 10, 0xecec13],
+            ["I", "NVENT", 10, 0x7cb30f],
+            ["C", "ATALYSE", 10, 0x0a760a],
+            ["O", "PERATE", 10, 0x0a4076],
+            ["L", "OGIC", 10, 0x21313ec],
+            ["O", "RDER", 10, 0x300fb3],
+            ["U", "NITY", 10, 0x430d9c],
+            ["R", "OBOTICS", 10, 0x490a76],
         ];
         this.lines = new Array(background.length);
+        this.textColorRanges = new Array(background.length);
         for (let i = 0; i < background.length; ++i) {
             const args = background[i];
             const model = new Group();
@@ -264,7 +365,7 @@ export const flower = Macro(class Flower extends MacroElement {
                 left.position.x = width / -2;
                 this.lines[i].length = width;
                 const height = (bounds[3] - bounds[1]);
-                model.position.y = (background.length - i - 1) * height - height * background.length / 2;
+                model.position.y = (background.length - i - 1) * height - height * background.length / 2 + 0.03;
             });
             right.addEventListener("synccomplete", () => {
                 const bounds = right.textRenderInfo.blockBounds;
@@ -274,6 +375,9 @@ export const flower = Macro(class Flower extends MacroElement {
             this.lines[i] = {
                 model,
                 length: 0
+            };
+            this.textColorRanges[i] = {
+                left, right, colorRanges: left.colorRanges
             };
             this.scene.add(model);
         }
@@ -292,7 +396,7 @@ export const flower = Macro(class Flower extends MacroElement {
     createText(start, end, repetition, color) {
         const tmp = new Text();
         tmp.font = "./fonts/microgramma/microgramma.otf";
-        tmp.fontSize = 47 * SCENE_SCALE;
+        tmp.fontSize = 42 * SCENE_SCALE;
         tmp.textAlign = "center";
         tmp.anchorX = "center";
         tmp.anchorY = "bottom";
@@ -323,9 +427,17 @@ export const flower = Macro(class Flower extends MacroElement {
         this.bokeh.uniforms.aspect.value = 3440 / 1440;
         if (width > 1000) {
             this.root.position.x = -130 * SCENE_SCALE;
+            for (let i = 0; i < this.textColorRanges.length; ++i) {
+                this.textColorRanges[i].left.colorRanges = this.textColorRanges[i].colorRanges;
+                this.textColorRanges[i].right.colorRanges = this.textColorRanges[i].colorRanges;
+            }
         }
         else {
             this.root.position.x = -110 * SCENE_SCALE;
+            for (let i = 0; i < this.textColorRanges.length; ++i) {
+                this.textColorRanges[i].left.colorRanges = {};
+                this.textColorRanges[i].right.colorRanges = {};
+            }
         }
     }
     render() {
